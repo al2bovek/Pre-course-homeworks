8541600ec65e4a96162357e22a811dc6cc620024
Aleksandr Dubovik <95865961+al2bovek@users.noreply.github.com>
2024-01-02 10:59:17 +0200

    Update 03.js

--- lesson-01/03.js
+++ lesson-01/03.js
@@ -2,10 +2,10 @@
 let number = 1;
 const temp = [];
 temp.push(number++);
-console.log(temp.join(','));
+console.log(temp[0]);
 temp.push(++number);
-console.log(temp.join(','));
+console.log(temp[0], temp[1]);
 temp.push(number--);
-console.log(temp.join(','));
+console.log(temp[0], temp[1], temp[2]);
 temp.push(--number);
-console.log(temp.join(','));
+console.log(temp[0] + ',' + temp[1] + ',' + temp[2] + ',' + temp[3]);
