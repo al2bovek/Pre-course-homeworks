b131ad46e8fc12ef8903cf238122bf3fe2c9dbe2
Aleksandr Dubovik <95865961+al2bovek@users.noreply.github.com>
2024-01-14 13:42:23 +0200

    Add files via upload

---
+++ lesson-03/02.js
@@ -0,0 +1,5 @@
+const words = ["яблоко", "банан", "апельсин", "манго", "киви"];
+let 
+let orange;
+words.indexOf("апельсин") >= 0 ? orange = 'Ура! нашел' : orange = 'Придется поискать в другом магазине…';
+console.log(orange);
