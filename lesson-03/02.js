3bd3e42784de8ca3d7054f8d11d4092000e13c2c
Aleksandr Dubovik <95865961+al2bovek@users.noreply.github.com>
2024-01-14 13:47:36 +0200

    Update 02.js

--- lesson-03/02.js
+++ lesson-03/02.js
@@ -1,5 +1,4 @@
 const words = ["яблоко", "банан", "апельсин", "манго", "киви"];
-let 
 let orange;
 words.indexOf("апельсин") >= 0 ? orange = 'Ура! нашел' : orange = 'Придется поискать в другом магазине…';
 console.log(orange);
