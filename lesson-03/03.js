7e96f0ccc66464287a1263e63af01f4834b45a29
Aleksandr Dubovik <95865961+al2bovek@users.noreply.github.com>
2024-01-14 15:30:51 +0200

    Add files via upload

---
+++ lesson-03/03.js
@@ -0,0 +1,3 @@
+const fruits = ["яблоко", "банан", "апельсин", "манго", "киви", "ананас", "виноград", "груша", "слива", "персик"];
+const fruitsCut = fruits.slice(1, fruits.length - 2);
+console.log(fruitsCut);
